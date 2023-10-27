0085fc2d9eea3c528512758593823c32a2d2704d
Hilary Smith <143349665+Hilary-Smith-98@users.noreply.github.com>
2023-10-27 15:13:59 +0100

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -9,16 +9,16 @@ def test_get_homepage_not_logged_in(page, test_web_address):
 
 def test_get_homepage_logged_in(page, test_web_address):
     page.goto(f"http://{test_web_address}/login")
-    page.fill('input[name="Username"]', 'Silvakippy369')
-    page.fill('input[name="Password"]', '782993a')
+    page.fill('input[name="Username"]', 'Silvakippy')
+    page.fill('input[name="Password"]', 'securepassword')
     page.click('input[type="submit"]')
     tag = page.locator('Body')
     expect(tag).to_contain_text('List a new Property')
 
 def test_log_out(page, test_web_address):
     page.goto(f"http://{test_web_address}/login")
-    page.fill('input[name="Username"]', 'Silvakippy369')
-    page.fill('input[name="Password"]', '782993a')
+    page.fill('input[name="Username"]', 'Silvakippy')
+    page.fill('input[name="Password"]', 'securepassword')
     page.click('input[type="submit"]')
     page.get_by_test_id('Logout').click()
     tag = page.locator('Body')
